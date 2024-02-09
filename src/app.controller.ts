435f86409ccf830e6cb05cbe3faee7c00ca862a4
Gayaani <gayani.parameswaran@gmail.com>
2024-02-09 12:04:36 +0530

    changed 3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { InputText } from './dto/input-text.dto';
 
-@Controller()
+
+@Controller('sentiment')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/predict')
+  async predictSentiment(@Body() input: InputText) {
+    return this.appService.predictSentiment(input.text);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -11,19 +11,20 @@ export class AppService {
 
   constructor() {
     this.loadTokenizer();
-    this.loadModel();
+    this.loadModel('my_model.h5');;
   }
 
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello!';
   }
 
   async predictSentiment(text: string): Promise<string> {
     try {
       const tokens = this.tokenizer(text);
-      const paddedTokens = tf.pad(tf.tensor2d(tokens), [[0, 0], [0, 200 - tokens.length]]);
+      const paddedTokens = tf.pad(tf.tensor2d([tokens]), [[0, 0], [0, 200 - tokens.length]]);
       const prediction = this.model.predict(paddedTokens.reshape([1, 200]));
-      const predictedClassIndex = tf.argMax(prediction, 1).dataSync()[0];
+      // const combinedPrediction = tf.concat(prediction, 0); // Combine tensors in the array into a single tensor
+      const predictedClassIndex = tf.argMax(prediction[0]).dataSync()[0];
       const predictedSentiment = SentimentLabels[predictedClassIndex];
       console.log(`Predicted Sentiment: ${predictedSentiment}`);
       return predictedSentiment;
@@ -39,8 +40,18 @@ export class AppService {
     this.tokenizer = pickle.loads(dataBuffer);
   }
 
-  private loadModel(): void {
-    const modelPath = './model/model.json';
-    this.model = tf.node.loadGraphModel(modelPath);
+  // private loadModel(): void {
+  //   const modelPath = './model/model.json';
+  //   this.model = tf.node.loadGraphModel(modelPath);
+  // }
+  async loadModel(modelPath: string): Promise<void> {
+    try {
+        this.model = await tf.loadGraphModel(`file://${modelPath}`);
+        console.log('Model loaded successfully');
+    } catch (error) {
+        console.error('Failed to load model:', error);
+        throw new Error('Failed to load model.');
+    }
   }
+
 }
